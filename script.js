9d5c92e98ce8115f57411f8d137f50f39771c429
kareenap55 <kareenap1412@gmail.com>
2025-04-03 09:27:24 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // Qui
- choiceElement.setAttribute("name", `question-${i}`);       choiceElement.setAttribute("value", choice);       if (userAnswers[i] === choice) {         choiceElement.checked = true;         choiceElement.setAttribute("checked", "true");       }       choiceElement.addEventListener("change", () => { 		  saveAnswer(i, choice); 		  choiceElement.checked = true; 	  });         saveAnswer(i, choice);         choiceElement.setAttribute("checked", "true");       });        const label = document.createElement("label");       label.appendChild(choiceElement); 
+@@ -47,12 +47,12 @@ function renderQuestions() {
+       choiceElement.setAttribute("name", `question-${i}`);
+       choiceElement.setAttribute("value", choice);
+       if (userAnswers[i] === choice) {
+         choiceElement.checked = true;
+         choiceElement.setAttribute("checked", "true");
+       }
+       choiceElement.addEventListener("change", () => {
+ 		  saveAnswer(i, choice);
+ 		  choiceElement.checked = true;
+ 	  });
+         saveAnswer(i, choice);
+         choiceElement.setAttribute("checked", "true");
+       });
+ 
+       const label = document.createElement("label");
+       label.appendChild(choiceElement);
